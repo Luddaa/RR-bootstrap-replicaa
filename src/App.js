9fc82c72d5e57b7734184796fe35b5ca3d0a606f
Luddaa <dangeloeugene33@gmail.com>
2023-07-19 23:26:43 -0700

    finised files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import TopBar from './components/TopBar'
+import Background from './components/Background.js'
+import StoreListings from './components/StoreListings.js'
+
+
+function App() {
+  return (
+    <div className="App">
+      <TopBar />
+      <Background />
+      <StoreListings />
+    </div>
+  );
+}
+
+export default App;
